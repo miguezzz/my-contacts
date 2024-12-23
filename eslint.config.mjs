06be553e7a31303e4d5d51475ccef07807326bbc
Victor <v.miguez2004@gmail.com>
2024-12-23 01:25:31 -0300

    add eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
-  {
-    extends: ["arbnb-base"],
-  },
-  {
-    rules: {
-      "no-unused-vars": "warn",
-    },
-  },
 ];
