fe54321ac2d74de6732376d8d3fa9e98a9feb88d
Victor <v.miguez2004@gmail.com>
2024-12-23 01:19:16 -0300

    add eslint.config e .editorconfig

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const express = require("express"); // como é um pacote de node modules, não p
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+app.get("/", (request, response) => {
+  response.send("Hello World!");
 });
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => console.log("Server is running on port 3000"));
