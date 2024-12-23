97362e611b4de1cefe66ae4983e72b0fda7ac24d
Victor <v.miguez2004@gmail.com>
2024-12-23 18:48:14 -0300

    feat: rota store e middleware nas rotas (comment)

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,23 @@ const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
+/*
+Rota com middleware:
+router.get(
+  '/contacts',
+  (request, response, next()) => {
+    request.appID = '12345'; // adiciona uma propriedade ao objeto request no middleware
+    response.send('Interceptado pelo middleware');
+  },
+  (request, response, next()) => response.send('Outro middleware'),
+  ContactController.index),;
+*/
+
 router.get('/', (response) => response.send('Hello World!'));
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
+
+router.post('/contacts', ContactController.store);
 
 router.delete('/contacts/:id', ContactController.delete);
 
