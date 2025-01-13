ec0a2faf546fd30727d9edf2e517c96e842bdc75
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 16:43:58 -0300

    feat: ThemeProvider, default theme, global style, Sora font

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/themes/default';
+
 function App() {
-  return <h1>MyContacts</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>MyContacts</h1>
+    </ThemeProvider>
+  );
 }
 
 export default App;
