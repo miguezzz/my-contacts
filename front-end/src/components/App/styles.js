2ccbcdf66a3e2070a8750bc971c685d6a5cd5a96
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 20:34:41 -0300

    feat: app component and styled component

---
+++ front-end/src/components/App/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%; // ocupa 100% da largura da tela
+  max-width: 500px; // se o dispositivo tiver mais de 500px de largura, o container terá 500px
+  margin: 0 auto; // centraliza o container na tela
+`;
