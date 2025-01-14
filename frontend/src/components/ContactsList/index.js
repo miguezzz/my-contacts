2feb6b332aa49bac6a57cd11d75af961c0ef8826
Victor Miguez <v.miguez2004@gmail.com>
2025-01-13 23:51:46 -0300

    feat: contacts list header

---
+++ frontend/src/components/ContactsList/index.js
@@ -0,0 +1,12 @@
+import { Container, Header } from "./styles";
+
+export default function ContactsList() {
+  return (
+    <Container>
+      <Header>
+        <strong>3 Contatos</strong>
+        <a href="/">Adicionar Contato</a>
+      </Header>
+    </Container>
+  );
+}
