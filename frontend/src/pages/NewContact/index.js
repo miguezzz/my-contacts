e2ff7b15ae3fd45760830d1bd6c97141815bbb96
Victor Miguez <v.miguez2004@gmail.com>
2025-01-14 18:38:22 -0300

    feat: Input and Select components

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,5 +1,19 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
-  return <PageHeader title="Novo Contato" />;
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
+  );
 }
