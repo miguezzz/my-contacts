a8d988279a002bf2e93e4c2ec9b251ad6d1ddd76
Victor Miguez <v.miguez2004@gmail.com>
2025-01-14 22:31:00 -0300

    feat: ContactForm and FormGroup components

---
+++ frontend/src/components/ContactForm/index.js
@@ -0,0 +1,35 @@
+import { Form, ButtonContainer } from './styles';
+
+import FormGroup from '../FormGroup';
+import Input from '../Input';
+import Select from '../Select';
+import Button from '../Button';
+
+export default function ContactForm({ buttonLabel }) {
+  return (
+    <Form>
+      <FormGroup>
+        <Input placeholder="Nome" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input placeholder="E-mail" />
+      </FormGroup>
+
+      <FormGroup>
+        <Input placeholder="Telefone" />
+      </FormGroup>
+
+      <FormGroup>
+        <Select>
+          <option value="instagram">Instagram</option>
+          <option value="faculdade">Faculdade</option>
+        </Select>
+      </FormGroup>
+
+      <ButtonContainer>
+        <Button type="submit">{buttonLabel}</Button>
+      </ButtonContainer>
+    </Form>
+  );
+}
---
+++ frontend/src/components/FormGroup/index.js
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+export default function FormGroup({ children }) {
+  return <Container>{children}</Container>;
+}
