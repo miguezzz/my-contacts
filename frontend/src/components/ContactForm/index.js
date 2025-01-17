416207852777e1f52c9cbf6a6d3e2eee494d567a
Victor Miguez <v.miguez2004@gmail.com>
2025-01-17 01:08:58 -0300

    feat: name input validation

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -12,6 +12,30 @@ export default function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    // usamos o event.target.value ao invés de name, por conta da assincronicidade do setState
+    if (!event.target.value) {
+      setErrors((prevErrors) => [
+        ...prevErrors,
+        {
+          field: 'name',
+          message: 'Campo obrigatório',
+        },
+      ]);
+    } else {
+      // se o campo name estiver preenchido, queremos remover erro com field name
+      // filtra todos os erros cujo field seja diferente de name
+      setErrors((prevErrors) =>
+        prevErrors.filter((error) => error.field !== 'name'),
+      );
+    }
+  }
+
+  console.log(errors);
 
   function handleSubmit(event) {
     event.preventDefault(); // previne o comportamento padrão do formulário (recarregar a página)
@@ -28,11 +52,7 @@ export default function ContactForm({ buttonLabel }) {
     <Form onSubmit={handleSubmit}>
       <FormGroup>
         {/* O valor do input é controlado pelo estado name */}
-        <Input
-          placeholder="Nome"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <Input placeholder="Nome" value={name} onChange={handleNameChange} />
       </FormGroup>
 
       <FormGroup>
