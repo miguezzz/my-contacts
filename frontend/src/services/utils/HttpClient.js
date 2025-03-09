0b1bd5051a8f503636f65dd4d1d5864c7dfec82b
Victor Miguez <v.miguez2004@gmail.com>
2025-03-09 19:14:02 -0300

    feat: assure content-type is application/json before converting it to JSON. this is nice because avoids errors converting html code to json (when api returns errors)

--- frontend/src/services/utils/HttpClient.js
+++ frontend/src/services/utils/HttpClient.js
@@ -6,7 +6,11 @@ class HttpClient {
   async get(path) {
     const response = await fetch(`${this.baseUrl}${path}`);
 
-    const body = await response.json();
+    let body = null;
+    const contentType = response.headers.get('Content-Type');
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
 
     if (response.ok) {
       return body;
